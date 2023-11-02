af6e39efbed4cc0bd38b5dec702cbaa4723ee0ba
Aman Maurya <84094140+hackman01@users.noreply.github.com>
2023-11-02 12:05:09 -0400

    Update settings.py
    changed secret

--- fruit_basket_2101100100006/fruit_basket_2101100100006/settings.py
+++ fruit_basket_2101100100006/fruit_basket_2101100100006/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h0h_7g1y4*v=n8v$2uutl8y380$t+d@7d!igza@6s5#vk8n#8('
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
